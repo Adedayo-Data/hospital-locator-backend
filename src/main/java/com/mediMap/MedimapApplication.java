4a6993d39d6dd02a6086ee9a5090727ac9464897
Adedayo-Data <theophilusdayo@outlook.com>
2025-07-20 15:01:48 +0100

    Initial commit

---
+++ src/main/java/com/mediMap/MedimapApplication.java
@@ -0,0 +1,13 @@
+package com.mediMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedimapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedimapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mediMap/MedimapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedimapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
